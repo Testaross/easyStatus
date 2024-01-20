c242e0c707bd71384600355c8e9fe53c3a628e1f
Testarossa <austin.w.burrows@gmail.com>
2024-01-20 18:56:39 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,8 +7,7 @@ use_experimental_fxv2_oal 'yes'
 shared_script { 'config.lua'}
 
 client_scripts {
-    'client.lua',
-	'bridge/brrrridge.lua'
+    'client.lua', 'bridge/brrrridge.lua'
 }
 
 server_scripts {
